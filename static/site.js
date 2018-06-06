80276d0cfae23d779b907ce8688db0aab67d18f1
Richard Dalton <richard@devjoy.com>
2018-06-06 08:41:49 +0100

    Get random boards generated in Python to display using SVG

--- static/site.js
+++ static/site.js
@@ -1,3 +1,38 @@
+var board_positions = {
+    0: [0, 0],
+    1: [100, 0],
+    2: [200, 0],
+    3: [0, 100],
+    4: [100, 100],
+    5: [200, 100],
+    6: [0, 200],
+    7: [100, 200],
+    8: [200, 200],
+};
+
 function click_square(num) {
     alert(num)
 }
+
+function show_piece(piece, position) {
+    var  svgns = "http://www.w3.org/2000/svg";
+    var  xlinkns = "http://www.w3.org/1999/xlink";
+
+    var  g = document.getElementById("board");
+    var  use = document.createElementNS(svgns, "use");
+
+    if (piece == 1) {
+        use.setAttributeNS(xlinkns, "href", "#x-piece");
+    } else if (piece == -1) {
+        use.setAttributeNS(xlinkns, "href", "#o-piece");
+    }
+
+    use.setAttribute("x", position[0]);
+    use.setAttribute("y", position[1]);
+    g.appendChild(use);
+}
+
+
+Object.keys(board_pieces).forEach(function(key) {
+    show_piece(board_pieces[key], board_positions[key]);
+});
--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,13 @@
-import os
 from flask import Flask, redirect, render_template, request
+import json
+from random import choice
 
 app = Flask(__name__)
 
 @app.route("/")
 def get_index():
-    return render_template("index.html")
+    board_pieces = { i: choice([-1, 0, 1]) for i in range(9)}
+    return render_template("index.html", board_pieces = json.dumps(board_pieces))
 
 if __name__ == '__main__':
     app.run(debug=True)
