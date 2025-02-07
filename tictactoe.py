0bdb2c8870afc2236a7256eb167ff082246114c7
Richard Dalton <richard@devjoy.com>
2025-02-07 12:02:18 +0000

    Refactor api code

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,6 @@
 from os import abort
 
-from flask import Flask, request, abort, render_template, jsonify
+from flask import Flask, request, abort, redirect, jsonify
 from flask_cors import CORS, cross_origin
 from tictactoe_minimax import TicTacToeMiniMax
 from constants import *
@@ -10,6 +10,21 @@ app = Flask(__name__)
 cors = CORS(app)
 
 BASE_URL = "http://127.0.0.1:5000"
+
+
+def parse_request(request):
+    if 'board' in request.args:
+        board_str = request.args['board']
+    else:
+        abort(400, "board not provided")
+
+    if 'player' in request.args:
+        player_str = request.args['player']
+    else:
+        abort(400, "player not provided")
+
+    return  str_to_player(player_str), str_to_board(board_str)
+
 
 # Map between the web based string representation of the player and board, and the algorithm representation
 def str_to_player(str):
@@ -27,7 +42,7 @@ def player_to_str(player):
         X: 'x',
         O: 'o',
     }
-    return player_to_str(player)
+    return map_player_to_str[player]
 
 
 def str_to_board(str):
@@ -50,14 +65,13 @@ def board_to_str(board):
 
 # Use AI to get move for a given position
 def get_move(board, player):
-    board2 = str_to_board(board)
     ai = TicTacToeMiniMax()
-    move = ai.get_move(board2, player)
-    board2[move] = O
-    return board2
+    move = ai.get_move(board, player)
+    board[move] = O
+    return board
 
 
-def api_get_moves(board, player):
+def api_get_move_urls(board, player):
     moves = {}
     for i in range(9):
         if board[i] == 'b':
@@ -66,78 +80,52 @@ def api_get_moves(board, player):
     return moves
 
 
-@app.route("/api/new")
-@cross_origin()
-def api_new_game():
-    player = 'x'
-    board = 'bbbbbbbbb'
-    moves = api_get_moves(board, player)
-
+def serialize_response(player, board, result, moves):
     response = {
         'player': player,
         'board': board,
-        'result': None,
-        'links': {
-            'new_game': BASE_URL + '/api/new',
-            'moves': moves,
-        }
-    }
-    return jsonify(response)
-
-
-@app.route("/api/move")
-@cross_origin()
-def api_move():
-    player_str, board_str = parse_request(request)
-
-    player = str_to_player(player_str)
-    oplayer = other_player(player)
-    board = get_move(board_str, oplayer)
-
-    str = board_to_str(board)
-    moves = api_get_moves(str, 'x')
-
-    result = score(board)
-    if result is not None:
-        moves = []
-
-    response = {
-        'player': 'x',
-        'board': str,
         'result': result,
         'links': {
             'new_game': BASE_URL + '/api/new',
             'moves': moves,
         }
-
     }
     return jsonify(response)
 
-def get_moves(board, player):
-    moves = {}
-    for i in range(9):
-        if board[i] == 'b':
-            moves[i] = board[:i] + player + board[i+1:]
-    return moves
+
+@app.route("/api/new")
+@cross_origin()
+def api_new_game():
+    player = 'x'
+    board = 'bbbbbbbbb'
+    result = None
+    moves = api_get_move_urls(board, player)
+    return serialize_response(player, board, result, moves)
+
+
+@app.route("/api/move")
+@cross_origin()
+def api_move():
+    player, board = parse_request(request)
+
+    otherplayer = other_player(player)
+
+    # Get the other players move (AI)
+    board = get_move(board, otherplayer)
+
+    player_str = player_to_str(player)
+    board_str = board_to_str(board)
+    moves = api_get_move_urls(board_str, player_str)
+
+    result = score(board)
+    if result is not None:
+        moves = []
+    return serialize_response(player_str, board_str, result, moves)
 
 
 @app.route("/")
 def get_index():
-    return render_template("index.html")
-
-
-def parse_request(request):
-    if 'board' in request.args:
-        board_str = request.args['board']
-    else:
-        abort(400, "board not provided")
-
-    if 'player' in request.args:
-        player_str = request.args['player']
-    else:
-        abort(400, "player not provided")
-
-    return player_str, board_str
+    return redirect("/api/new")
 
 
 if __name__ == '__main__':
